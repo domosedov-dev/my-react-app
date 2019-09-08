3c77e79d0b46984970e52f9be64de4a18b06046d
Aleksandr Grigorii <domosedov.dev@gmail.com>
2019-09-08 12:18:01 +0300

    lifecycle

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,94 +3,68 @@ import * as axios from 'axios';
 import style from './Users.module.css';
 import avatar from '../../assets/images/avatar.png';
 
-const Users = props => {
-  if (props.users.length === 0) {
+class Users extends React.Component {
+  componentDidMount() {
     axios
       .get('https://social-network.samuraijs.com/api/1.0/users')
       .then(response => {
-        props.setUsers(response.data.items);
+        this.props.setUsers(response.data.items);
       });
-
-    // props.setUsers([
-    //   {
-    //     id: 1,
-    //     avatarURL:
-    //       'https://sun9-7.userapi.com/c850420/v850420791/197dca/5gv1wntX7pE.jpg?ava=1',
-    //     followed: false,
-    //     fullName: 'Alex',
-    //     status: "Hello, I'm domosedov!",
-    //     location: { city: 'Moscow', country: 'Russia' }
-    //   },
-    //   {
-    //     id: 2,
-    //     avatarURL:
-    //       'https://sun1-83.userapi.com/c845322/v845322463/1fbe41/oD8txtk8Bkc.jpg?ava=1',
-    //     followed: false,
-    //     fullName: 'Sonya',
-    //     status: 'Sofik - popik',
-    //     location: { city: 'Moscow', country: 'Russia' }
-    //   },
-    //   {
-    //     id: 3,
-    //     avatarURL:
-    //       'https://sun9-2.userapi.com/c850120/v850120482/1d59e3/phS7Sqr0ztc.jpg?ava=1',
-    //     followed: true,
-    //     fullName: 'Yuriy',
-    //     status: 'Loh',
-    //     location: { city: 'Saratov', country: 'Russia' }
-    //   }
-    // ]);
   }
 
-  return (
-    <div>
-      {props.users.map(user => {
-        return (
-          <div key={user.id}>
-            <span>
-              <div>
-                <img
-                  className={style.avatar}
-                  src={user.photos.small !== null ? user.photos.small : avatar}
-                  alt={user.name}
-                />
-              </div>
-              <div>
-                {user.followed ? (
-                  <button
-                    onClick={() => {
-                      props.unFollow(user.id);
-                    }}
-                  >
-                    Unfollow
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => {
-                      props.follow(user.id);
-                    }}
-                  >
-                    Follow
-                  </button>
-                )}
-              </div>
-            </span>
-            <span>
+  render() {
+    return (
+      <div>
+        {this.props.users.map(user => {
+          return (
+            <div key={user.id}>
               <span>
-                <div>{user.name}</div>
-                <div>{user.status}</div>
+                <div>
+                  <img
+                    className={style.avatar}
+                    src={
+                      user.photos.small !== null ? user.photos.small : avatar
+                    }
+                    alt={user.name}
+                  />
+                </div>
+                <div>
+                  {user.followed ? (
+                    <button
+                      onClick={() => {
+                        this.props.unFollow(user.id);
+                      }}
+                    >
+                      Unfollow
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => {
+                        this.props.follow(user.id);
+                      }}
+                    >
+                      Follow
+                    </button>
+                  )}
+                </div>
               </span>
               <span>
-                <div>{'user.location.country'}</div>
-                <div>{'user.location.city'}</div>
+                <span>
+                  <div>{user.name}</div>
+                  <div>{user.status}</div>
+                </span>
+                <span>
+                  <div>{'user.location.country'}</div>
+                  <div>{'user.location.city'}</div>
+                </span>
               </span>
-            </span>
-            <hr />
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+              <hr />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
 
 export default Users;
